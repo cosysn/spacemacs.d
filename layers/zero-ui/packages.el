d4ff7791691a29e3dbffac9f03f79edd8cca3e93
cosysn <cosysn@163.com>
2017-04-10 07:29:17 +0800

    first commit

---
+++ layers/zero-ui/packages.el
@@ -0,0 +1,101 @@
+;;; packages.el --- zero-ui layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author:  <Cosysn@Cosysn-PC>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;; See the Spacemacs documentation and FAQs for instructions on how to implement
+;; a new layer:
+;;
+;;   SPC h SPC layers RET
+;;
+;;
+;; Briefly, each package to be installed or configured by this layer should be
+;; added to `zero-ui-packages'. Then, for each package PACKAGE:
+;;
+;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
+;;   function `zero-ui/init-PACKAGE' to load and initialize the package.
+
+;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
+;;   define the functions `zero-ui/pre-init-PACKAGE' and/or
+;;   `zero-ui/post-init-PACKAGE' to customize the package as it is loaded.
+
+;;; Code:
+
+(defconst zero-ui-packages
+  '(
+    (whitespace :location built-in)
+    )
+  "The list of Lisp packages required by the zero-ui layer.
+
+Each entry is either:
+
+1. A symbol, which is interpreted as a package to be installed, or
+
+2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
+    name of the package to be installed or loaded, and KEYS are
+    any number of keyword-value-pairs.
+
+    The following keys are accepted:
+
+    - :excluded (t or nil): Prevent the package from being loaded
+      if value is non-nil
+
+    - :location: Specify a custom installation location.
+      The following values are legal:
+
+      - The symbol `elpa' (default) means PACKAGE will be
+        installed using the Emacs package manager.
+
+      - The symbol `local' directs Spacemacs to load the file at
+        `./local/PACKAGE/PACKAGE.el'
+
+      - A list beginning with the symbol `recipe' is a melpa
+        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
+
+
+
+(defun zero-ui/post-init-whitespace ()
+  (progn
+    ;; http://emacsredux.com/blog/2013/05/31/highlight-lines-that-exceed-a-certain-length-limit/
+    (setq whitespace-line-column fill-column) ;; limit line length
+;;https://www.reddit.com/r/emacs/comments/2keh6u/show_tabs_and_trailing_whitespaces_only/
+    (setq whitespace-display-mappings
+          ;; all numbers are Unicode codepoint in decimal. try (insert-char 182 ) to see it
+          '(
+            (space-mark 32 [183] [46])           ; 32 SPACE, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
+            (newline-mark 10 [182 10])           ; 10 LINE FEED
+            (tab-mark 9 [187 9] [9655 9] [92 9]) ; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」
+            ))
+    (setq whitespace-style '(face tabs trailing tab-mark ))
+    ;; (setq whitespace-style '(face lines-tail))
+    ;; show tab;  use untabify to convert tab to whitespace
+    (setq spacemacs-show-trailing-whitespace nil)
+
+    (setq-default tab-width 4)
+    ;; set-buffer-file-coding-system -> utf8 to convert dos to utf8
+    ;; (setq inhibit-eol-conversion t)
+    ;; (add-hook 'prog-mode-hook 'whitespace-mode)
+
+    ;; (global-whitespace-mode +1)
+
+    (with-eval-after-load 'whitespace
+      (progn
+        (set-face-attribute 'whitespace-tab nil
+                            :background "#Adff2f"
+                            :foreground "#00a8a8"
+                            :weight 'bold)
+        (set-face-attribute 'whitespace-trailing nil
+                            :background "#e4eeff"
+                            :foreground "#183bc8"
+                            :weight 'normal)))
+    (diminish 'whitespace-mode)))
+
+;;; packages.el ends here
